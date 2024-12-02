bf15c8403183992a92159ced31bf4e976830e283
Yanda Bao <yandabaodav@gmail.com>
2024-12-02 10:19:08 -0800

    initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+from pinecone import Pinecone, ServerlessSpec
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+pc = Pinecone(api_key=os.getenv("PINECONE_API_KEY"))
+
+index_name = "FitLabels"
+
+pc.create_index(
+    name=index_name,
+    dimension=2, # Replace with your model dimensions
+    metric="cosine", # Replace with your model metric
+    spec=ServerlessSpec(
+        cloud="aws",
+        region="us-east-1"
+    ) 
+)
+
+print(pc)
